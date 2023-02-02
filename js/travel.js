2bef7572107829b919aaa711ab82b96d8eec1b1b
guiyee <guillermodinanno@gmail.com>
2023-02-02 15:18:57 -0300

    pruebaTravel

--- js/travel.js
+++ js/travel.js
@@ -27,24 +27,7 @@
 
 
 
-{/* <section class="grid_container_travel">
-                
-                <article class="grid_travel_col_1 flex_item_travel" >
-                    <div class="travel_img">
-                        <img class="zoom_1" src="../image/sydney.jpg" alt="Sydney Opera House">
-                        <img  class="zoom_2" src="../image/sydney2.jpg" alt="Travelers enjoying Sydney's views">
-                    </div>
-                    <div class="travel_text_buybutton" >
-                        <h2>SYDNEY</h2>
-                        <p>Whether you visit Australia for business or leisure, Four Points by Sheraton Sydney, Central Park makes your stay worthwhile. Stay and enjoy excellent service, thoughtful amenities and a prime location in Sydney's central Chippendale neighbourhood. Our hotel is near Sydney CBD, Central Station, the ICC Sydney and numerous shops and restaurants.</p>
-                        <p class="p">For those organising events, our light-filled venues are tailor-made for both corporate functions and social celebrations in Sydney. Relax in modern hotel rooms and suites featuring our signature bedding, large bathrooms with rainforest showers, mini-refrigerators, ample desks and free Wi-Fi, paired with views over Chippendale. Our hotel suites boast large, separate living rooms, ideal for families or business trips.</p>
-                        
-                       <!--  <a href="https://www.esky.com/?gclid=Cj0KCQiA37KbBhDgARIsAIzce144bzlqBmBOgB-FA-JVXY8oYpPYpL2Bd9KjtGmnCRSQgVhxWE3SilIaAi3_EALw_wcB#info" target="_blank" class="button_travel"> --><!-- <button id="brisbane">
-                            <span class="button_top"> Purchase Travel Pack
-                            </span>
-                            </button> -->
-                        <!-- </a> --> 
-                    </div> */}
+
 
 
 
