833434218fefa3b680434f5380baed158824ff05
guiyee <guillermodinanno@gmail.com>
2023-02-02 15:36:46 -0300

    branchTravel

--- js/travel.js
+++ js/travel.js
@@ -1,28 +1,39 @@
 
-// const asd = document.querySelector("grid_container_travel")
+const section = document.querySelector(".grid_container_travel")
 
-// const section = document.createElement("article")
-// section.classList.add("grid_travel_col_1")
-// section.classList.add("flex_item_travel")
-// console.log(section)
-// section.innerHTML = `
-// <div class="travel_img">
-// <img class="zoom_1" src="../image/sydney.jpg" alt="Sydney Opera House">
-// <img  class="zoom_2" src="../image/sydney2.jpg" alt="Travelers enjoying Sydney's views">
-// </div>
-// <div class="travel_text_buybutton" >
-// <h2>SYDNEY</h2>
-// <p>Whether you visit Australia for business or leisure, Four Points by Sheraton Sydney, Central Park makes your stay worthwhile. Stay and enjoy excellent service, thoughtful amenities and a prime location in Sydney's central Chippendale neighbourhood. Our hotel is near Sydney CBD, Central Station, the ICC Sydney and numerous shops and restaurants.</p>
-// <p class="p">For those organising events, our light-filled venues are tailor-made for both corporate functions and social celebrations in Sydney. Relax in modern hotel rooms and suites featuring our signature bedding, large bathrooms with rainforest showers, mini-refrigerators, ample desks and free Wi-Fi, paired with views over Chippendale. Our hotel suites boast large, separate living rooms, ideal for families or business trips.</p>
+const article = document.createElement("article")
+article.classList.add("grid_travel_col_1")
+article.classList.add("flex_item_travel")
+console.log(article)
+article.innerHTML = `
+<div class="travel_img">
+    <img class="zoom_1" src="../image/sydney.jpg" alt="Sydney Opera House">
+    <img  class="zoom_2" src="../image/sydney2.jpg" alt="Travelers enjoying Sydney's views">
+</div>
+<div class="travel_text_buybutton" >
+    <h2>SYDNEY</h2>
+    <p>Whether you visit Australia for business or leisure, Four Points by Sheraton Sydney, Central Park makes your stay worthwhile. Stay and enjoy excellent service, thoughtful amenities and a prime location in Sydney's central Chippendale neighbourhood. Our hotel is near Sydney CBD, Central Station, the ICC Sydney and numerous shops and restaurants.</p>
+    <p class="p">For those organising events, our light-filled venues are tailor-made for both corporate functions and social celebrations in Sydney. Relax in modern hotel rooms and suites featuring our signature bedding, large bathrooms with rainforest showers, mini-refrigerators, ample desks and free Wi-Fi, paired with views over Chippendale. Our hotel suites boast large, separate living rooms, ideal for families or business trips.
+    </p>
+    <button id="brisbane">
+        <span class="button_top"> Purchase Travel Pack</span>
+    </button>
+    </div>
+    `
+section.append(article)
 
-// <!--  <a href="https://www.esky.com/?gclid=Cj0KCQiA37KbBhDgARIsAIzce144bzlqBmBOgB-FA-JVXY8oYpPYpL2Bd9KjtGmnCRSQgVhxWE3SilIaAi3_EALw_wcB#info" target="_blank" class="button_travel"> --><!-- <button id="brisbane">
-//     <span class="button_top"> Purchase Travel Pack
-//     </span>
-//     </button> -->
-// <!-- </a> --> 
-// </div>
-// `
-
+    // < div class="travel_img" >
+    //         <img class="zoom_1" src="../image/sydney.jpg" alt="Sydney Opera House">
+    //         <img class="zoom_2" src="../image/sydney2.jpg" alt="Travelers enjoying Sydney's views">
+    // </div>
+    // <div class="travel_text_buybutton" >
+    //         <h2>SYDNEY</h2>
+    //         <p>Whether you visit Australia for business or leisure, Four Points by Sheraton Sydney, Central Park makes your stay worthwhile. Stay and enjoy excellent service, thoughtful amenities and a prime location in Sydney's central Chippendale neighbourhood. Our hotel is near Sydney CBD, Central Station, the ICC Sydney and numerous shops and restaurants.</p>
+    //         <p class="p">For those organising events, our light-filled venues are tailor-made for both corporate functions and social celebrations in Sydney. Relax in modern hotel rooms and suites featuring our signature bedding, large bathrooms with rainforest showers, mini-refrigerators, ample desks and free Wi-Fi, paired with views over Chippendale. Our hotel suites boast large, separate living rooms, ideal for families or business trips.</p>
+    //         <button id="brisbane">
+    //             <span class="button_top"> Purchase Travel Pack</span>
+    //         </button>
+    // </div>
 
 
 
